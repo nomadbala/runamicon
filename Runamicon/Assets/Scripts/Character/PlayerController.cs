781aae5d320c599ee1a487fdcee5144b765ffe33
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-10-31 12:53:27 +0600

    AnimationsV4Jump

--- Runamicon/Assets/Scripts/Character/PlayerController.cs
+++ Runamicon/Assets/Scripts/Character/PlayerController.cs
@@ -30,8 +30,9 @@ public class PlayerController : MonoBehaviour {
 	private float _mouseAxisY;
 
 	private bool _isRun;
-	private bool _isAtackJump;
+	private bool _isJumpAtack;
 	private bool _isAttack;
+	private bool _isJump;
 
 	//private Direction _direction;
 	private Quaternion _targetRotation;
@@ -49,48 +50,32 @@ public class PlayerController : MonoBehaviour {
 	private void Update() {
 		_horizontalInput = Input.GetAxisRaw("Horizontal");
 		_verticalInput = Input.GetAxisRaw("Vertical");
-		Debug.Log(_horizontalInput + "   " + _verticalInput);
+		//Debug.Log(_horizontalInput + "   " + _verticalInput);
 		//_mouseAxisX = Input.GetAxis("Mouse X");
 		//_mouseAxisY = Input.GetAxis("Mouse Y");
-		if (Input.GetKeyDown(KeyCode.Mouse0) && _isAttack == false) {
-			_isAttack = true;
-			_animator.SetTrigger("IsAttack");
 
-			if (_verticalInput < 0) {
-				_animator.Play("AttackWithBackWalk", 2, 0f);
-				_animator.Play("FullRotation", 1, 0f);
-			} else if (!_isRun && _verticalInput >= 0 || _horizontalInput > 0) {
-				float time = 0f;
-				if (_animator.GetFloat("Speed") > 0f) {
-					time = 0.25f;
-					_animator.SetFloat("Speed", 0);
-					StopPlayerHorizontally();
-				}
-				//_animator.Play("Idle", 0, 0f);
-				Invoke("ForwardAttack", time);
-
-			} else if (_isRun && _verticalInput >= 0) {
-				if (_characterController.isGrounded) {
-					_isAtackJump = true;
-					_animator.Play("AttackWithForwardRun", 2, 0f);
-					//_characterController.Move(new Vector3(0f, _newPosition.y, 0f));
-				}
-			}
-		}
-		Debug.Log(_isAttack);
+		Attack();
 		Move();
 		//Rotation();
 	}
+	private void setJumpAttackMarker() {
+		_isJumpAtack = true;
+	}
+
 	private void ForwardAttack() {
 		_animator.Play("ForwardAttack", 2, 0f);
 		_animator.Play("NotFullRotation", 1, 0f);
+	}
+	private void BackwardAttack() {
+		_animator.Play("AttackWithBackWalk", 2, 0f);
+		_animator.Play("FullRotation", 1, 0f);
 	}
 
 	private void Rotation() {
 
 		//rotationX += _horizontalInput * 300 * Time.deltaTime;
 		//rotationX = Mathf.Clamp(rotationX, 0, 360); // Ограничение угла поворота
-		Debug.Log(_horizontalInput + "   " + _verticalInput);
+		//Debug.Log(_horizontalInput + "   " + _verticalInput);
 		//if (_verticalInput > 0.1 || _verticalInput < 0.1) {
 
 		//	if (rotationX > 0) {
@@ -112,38 +97,87 @@ public class PlayerController : MonoBehaviour {
 
 		if (!_isAttack) {
 			float speed = _isRun ? _runSpeed : _walkSpeed;
-			Vector3 direction = transform.right * _horizontalInput +
-													transform.forward * _verticalInput;
+			Vector3 direction = _player.transform.right * _horizontalInput +
+													_player.transform.forward * _verticalInput;
 			direction.Normalize();
 
 			_newPosition.x = direction.x * speed;
 			_newPosition.z = direction.z * speed;
 		}
+
 		JumpAndGravitation();
 		_characterController.Move(_newPosition * Time.deltaTime);
 
 		//Debug.Log(_characterController.velocity.magnitude);
 		if (!_isAttack) {
-			_animator.SetFloat("Speed", _characterController.velocity.magnitude);
-			_animator.SetFloat("VerticalDirections", _verticalInput);
-			_animator.SetFloat("HorizontalDirections", _horizontalInput);
-			_animator.SetBool("IsRun", _isRun);
+			SetAnimatorProperties();
 		}
+	}
+	private void Attack() {
+		if (Input.GetKeyDown(KeyCode.Mouse0) && _isAttack == false && !_isJump) {
+			_isAttack = true;
+
+			if (_verticalInput < 0) {
+				_animator.Play("AttackWithBackWalk", 2, 0f);
+				_animator.Play("FullRotation", 1, 0f);
+			} else if (!_isRun && (_verticalInput >= 0 || _horizontalInput != 0)) {
+				float time = 0f;
+				if (_animator.GetFloat("Speed") > 0f) {
+					time = 0.25f;
+					_animator.SetFloat("Speed", 0);
+					StopPlayerHorizontally();
+				}
+				//_animator.Play("Idle", 0, 0f);
+				Invoke("ForwardAttack", time);
+
+			} else if (_isRun && _verticalInput >= 0 && _horizontalInput == 0) {
+				if (_characterController.isGrounded) {
+					Invoke("setJumpAttackMarker", 0.3f);
+
+					_animator.Play("AttackWithForwardRun", 2, 0f);
+				}
+			} else if (_isRun && _horizontalInput != 0) {
+				float time = 0f;
+				if (_animator.GetFloat("Speed") > 0f) {
+					time = 0.5f;
+					_animator.SetFloat("Speed", 0);
+					_animator.SetBool("IsRun", false);
+					StopPlayerHorizontally();
+				}
+				Invoke("ForwardAttack", time);
+			} else {
+				_isAttack = false;
+			}
+		}
+	}
+	private void SetAnimatorProperties() {
+		_animator.SetFloat("Speed", _characterController.velocity.magnitude);
+		_animator.SetFloat("VerticalDirections", _verticalInput);
+		_animator.SetFloat("HorizontalDirections", _horizontalInput);
+		_animator.SetBool("IsRun", _isRun);
+
 	}
 	private void JumpAndGravitation() {
 		if (_characterController.isGrounded) {
-			if (Input.GetKeyDown(KeyCode.Space) || _isAtackJump) {
+			if ((Input.GetKeyDown(KeyCode.Space) && !_isAttack) || _isJumpAtack) {
+
+				if (_isJumpAtack) {
+					_isJumpAtack = false;
+				} else {
+					_isJump = true;
+					_animator.SetTrigger("IsJump");
+				}
 				_newPosition.y = _jumpHeight;
 			} else {
+				_isJump = false;
 				_newPosition.y = -_stickToGroundForce;
 			}
 
 		} else {
-			_isAtackJump = false;
+			//_isJumpAtack = false;
 			_newPosition.y += Physics.gravity.y * _gravityMultiplier * Time.deltaTime;
 		}
 	}
-
 	public void SetEndOfAttack() {
 		_isAttack = false;
 	}
