9c04d99cc8cd2109c1497f7610c823191d81c449
Ilya Kanishev <105855090+kanishevilya@users.noreply.github.com>
2023-11-04 19:57:12 +0600

    ChestOpening_andVisualLoot

--- Runamicon/Assets/Scripts/Character/LootSensor/LootSensor.cs
+++ Runamicon/Assets/Scripts/Character/LootSensor/LootSensor.cs
@@ -6,6 +6,8 @@ public class LootSensor : MonoBehaviour {
 	[SerializeField] private LayerMask _layerMask;
 	[SerializeField] private Transform _target;
 	private Ray _ray;
+
+	private bool _isDownActionKey;
 	private void Awake() {
 		_ray = new Ray(_target.position, _target.forward);
 	}
@@ -16,21 +18,29 @@ public class LootSensor : MonoBehaviour {
 	private void OnTriggerExit(Collider other) {
 		//Debug.Log("exit");
 	}
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.E)) {
+			_isDownActionKey = true;
+		}
+		if(Input.GetKeyUp(KeyCode.E)){
+
+			_isDownActionKey = false;
+		}
+	}
 	private void OnTriggerStay(Collider other) {
 		
-		if (other.gameObject.layer != 10) { return; }
+		if (other.gameObject.layer != 10 || !_isDownActionKey) { return; }
+		_isDownActionKey = false;
 
 		_ray.origin = _target.position;
 		_ray.direction = _target.forward;
 
-		if (Input.GetKey(KeyCode.E)) {
-			Debug.Log("E");
-			if (Physics.Raycast(_ray, out RaycastHit hitInfo, 2.5f, _layerMask)) {
-				//Debug.Log(hitInfo.transform.gameObject.name);
-				Interactable interactable = hitInfo.collider.gameObject.GetComponent<Interactable>();
-				if (interactable) {
-					interactable.Action();
-				}
+
+
+		if (Physics.Raycast(_ray, out RaycastHit hitInfo, 20.5f, _layerMask)) {
+			Interactable interactable = hitInfo.collider.gameObject.GetComponent<Interactable>();
+			if (interactable) {
+				interactable.Action();
 			}
 		}
 		// RaycastHit[] raycastHits= Physics.RaycastAll(_ray, 2.5f, _layerMask);
